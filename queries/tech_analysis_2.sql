5929f63e0a7ad5e0f2493cde735446a935b8b1ef
Martin Kaminskyj <martin.kaminskyj@gmail.com>
2020-06-03 17:40:54 -0400

    finished tech analysis 2 + csv

--- queries/tech_analysis_2.sql
+++ queries/tech_analysis_2.sql
@@ -1,16 +1,13 @@
 -- Technical Analysis Deliverable 2: Mentorship Eligibility
 
-
-
---sample below
-SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary, de.to_date
-INTO emp_info
+-- Joining employees and titles tables
+SELECT e.emp_no, e.first_name, e.last_name, titles.title, titles.from_date, titles.to_date
+INTO mentorship_eligibility
 FROM employees as e 
-INNER JOIN salaries as s
-ON (e.emp_no = s.emp_no)
-INNER JOIN dep_employees as de
-on (e.emp_no = de.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
-	AND (de.to_date = '9999-01-01')
+INNER JOIN titles 
+ON (e.emp_no = titles.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (titles.to_date = '9999-01-01')
 ;
+
+SELECT * FROM mentorship_eligibility;
